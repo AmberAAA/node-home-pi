7838beedc8f2b8082a5970b8b3da6da6bf633358
AmberAAA <qe-d@163.com>
2018-05-14 19:01:27 +0800

    ....

--- switch.js
+++ switch.js
@@ -1,16 +1,20 @@
-let io = require('socket.io-client')('http://104.225.237.158:8081')
-let gpio = require('onoff').Gpio
-let led = new gpio(17, 'out')
+// let io = require('socket.io-client')('http://104.225.237.158:8081')
+// let gpio = require('onoff').Gpio
+// let led = new gpio(17, 'out')
 
-io.on('connect',function () {
-    console.log('on connet')
-    io.emit('test')
-})
+// io.on('connect',function () {
+//     console.log('on connet')
+//     io.emit('test')
+// })
 
-io.on('amber',function () {
-    console.log('amber')
-})
+// io.on('amber',function () {
+//     console.log('amber')
+// })
 
-io.on('light',function () {
+// io.on('light',function () {
+//     led.writeSync(led.readSync() ^ 1)
+// })
+
+setInterval(()=>{
     led.writeSync(led.readSync() ^ 1)
-})
+},1000)
