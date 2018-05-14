a7cbd8444187cbdf1dc91cfab75a44bde080bffa
AmberAAA <qe-d@163.com>
2018-05-14 19:02:57 +0800

    zzz

--- switch.js
+++ switch.js
@@ -1,6 +1,6 @@
 // let io = require('socket.io-client')('http://104.225.237.158:8081')
-// let gpio = require('onoff').Gpio
-// let led = new gpio(17, 'out')
+let gpio = require('onoff').Gpio
+let led = new gpio(17, 'out')
 
 // io.on('connect',function () {
 //     console.log('on connet')
